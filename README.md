57cb3faba986499d077885b9e3b381ebd8a6493f
Ido Lev <idolev@Ido-Lev.local>
2018-12-26 09:58:46 +0200

    updae publish

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 
 [![Coverage Status](https://coveralls.io/repos/github/iiddoo/python-sample-package/badge.svg?branch=dev)](https://coveralls.io/github/iiddoo/python-sample-package?branch=dev)
 
-# python-seed
+# python-example-package
 
-This is a starter repo for creating a new python package. Included are templates for standard files as well as best practices.
+This is a starter repo for creating a new python package.
 
-Update the setup.py file to reflect the proper PACKAGENAME and rename the PACKAGENAME to the name of the package
+## Tests
+
+Simply run: `pytest`
+
+
+## Code coverge
+
+Generate coverage report with: `py.test --cov=myPackage tests/`
+
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='python-sample-package',
@@ -6,9 +10,15 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
-    long_description=open('README.txt').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['numpy'],
     url='https://github.com/iiddoo/python-sample-package.git',
     author='Ido Lev',
-    author_email='myemail@example.com'
+    author_email='iiddoolleevv@gmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
